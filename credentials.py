3d7ef66b911cf70adb754969ff5f4f2a90d44b74
igihozo-stella <igihozo96@gmail.com>
2020-11-15 11:08:34 +0200

    create credential class and define its methods.

--- credentials.py
+++ credentials.py
@@ -1,3 +1,54 @@
-import random
-import string
-class Credentials
+class Credentials:
+    '''
+    Class for generating account credentials,passwords and save this information
+    '''
+    
+    credentials_list=[]
+
+    def __init__(self,site_name,account_name,password):
+        '''
+        method to define the properties each user object will hold
+        '''
+
+        self.site_name=site_name
+        self.account_name=account_name
+        self.password=password
+
+    def save_credentials(self):
+        """Method that saves credential objects into credentials_list"""
+        self.credentials_list.append(self)
+    
+    def delete_credential(self):
+        """Method which deletes a particular credential"""
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_name(cls, site_name):
+        """Method that takes in a name and returns a credential that matches that particular name
+        Args:
+            name: account_name that has a password
+        Returns:
+            The account_name ,site_name and it's corresponding Password
+        """
+
+        for credential in cls.credentials_list:
+            if credential.site_name == site_name:
+                return credential
+    
+    @classmethod
+    def credential_exists(cls, name):
+        """Method to check whether a credential exists
+        Args:
+        name: name of account to search whether it exists
+        boolean: True or False depending if the contatc exists
+        """
+
+        for credential in cls.credentials_list:
+            if credential.site_name == name:
+                return True
+        return False
+
+    @classmethod
+    def display_credentials(cls):
+        """Method which displays all current credentials"""
+        return cls.credentials_list
